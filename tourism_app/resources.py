1da770c78de67f454a5b639edea227918cefa318
Swotty <Sw0tty@yandex.ru>
2023-10-25 22:59:22 +0300

    Resources mod

--- tourism_app/resources.py
+++ tourism_app/resources.py
@@ -1,27 +1,7 @@
-
-
-LEVEL_TYPE = {
-    ('summer', "A"),
-    ('spring', "B"),
-    ('autumn', "C"),
-    ('winter', "D"), 
-}
-
+# MountainLevel resources
 HARDEST_TYPE = {
     ('A', "Приятная прогулка"),
     ('B', "Теплая одеждка будет кстате"),
     ('C', "Лучше идти группой"),
     ('D', "Невыносимое испытание"),
-}
-
-AUTUMN_HARDEST_TYPE = {
-    ('', ""),
-}
-
-SPRING_HARDEST_TYPE = {
-    ('', ""),
-}
-
-WINTER_HARDEST_TYPE = {
-    ('', ""),
 }
