31863ed61bbe797fcaabe5b0dab7a36a80d85f02
unknown <Sw0tty@yandex.ru>
2023-10-27 13:26:37 +0300

    Register models

--- tourism_app/admin.py
+++ tourism_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from tourism_app.models import Tourist, MountainPass, MountainCoords, Image, MountainLevel
+
+
+admin.site.register(Tourist)
+admin.site.register(MountainPass)
+admin.site.register(MountainCoords)
+admin.site.register(Image)
+admin.site.register(MountainLevel)
