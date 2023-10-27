80ffcef7a78423539c282bcbb493a12769e97e71
unknown <Sw0tty@yandex.ru>
2023-10-27 13:49:46 +0300

    Removing

--- tourism_app/views.py
+++ tourism_app/views.py
@@ -1,10 +1,2 @@
 from django.shortcuts import render
 from rest_framework import generics
-
-from tourism_app.models import Tourist
-from tourism_app.serializers import TouristSerializer
-
-
-class TouristAPIView(generics.CreateAPIView):
-    model = Tourist
-    serializer_class = TouristSerializer
