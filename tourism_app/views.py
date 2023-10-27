2cc636a64d4a901d672d191ca5184443c5a6e80f
unknown <Sw0tty@yandex.ru>
2023-10-27 13:42:00 +0300

    SubmitData

--- tourism_app/views.py
+++ tourism_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from tourism_app.models import Tourist
+from tourism_app.serializers import TouristSerializer
+
+
+class TouristAPIView(generics.CreateAPIView):
+    model = Tourist
+    serializer_class = TouristSerializer
