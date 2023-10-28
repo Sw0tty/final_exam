d74702c507429c04ac19df585121d7b8eb475499
Swotty <Sw0tty@yandex.ru>
2023-10-28 19:48:00 +0300

    endpoint rename

--- tourism/urls.py
+++ tourism/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from tourism_app.views import TouristAPIView
+
+from tourism_app.views import SubmitData
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/tourist/', TouristAPIView.as_view()),
+    path('api/v1/submit-mountain-pass/', SubmitData.as_view()),
 ]
