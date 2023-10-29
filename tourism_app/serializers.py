16648d6c45f23574299f845393b79cc4c9cbd5b8
Swotty <Sw0tty@yandex.ru>
2023-10-29 12:14:34 +0300

    Unique checking overwrite

--- tourism_app/serializers.py
+++ tourism_app/serializers.py
@@ -38,14 +38,21 @@ class MountainPassSerializer(serializers.ModelSerializer):
         model = MountainPass
         exclude = ('status',)
 
+    def get_tourist(self, user_email):
+        if Tourist.objects.filter(email=user_email):
+            return Tourist.objects.get(email=user_email)
+        return None
 
     def create(self, validated_data):
         user_data = validated_data.pop('user')
         coords_data = validated_data.pop('coords')
         level_data = validated_data.pop('level')
         images_data = validated_data.pop('images')
+        tourist = self.get_tourist(user_email=user_data['email'])
 
-        tourist = Tourist.objects.create(**user_data)      
+        if not tourist:
+            tourist = Tourist.objects.create(**user_data)   
+               
         mountain_pass = MountainPass.objects.create(user=tourist, **validated_data)    
         MountainCoords.objects.create(mountain_pass=mountain_pass, **coords_data)
         MountainLevel.objects.create(mountain_pass=mountain_pass, **level_data)
