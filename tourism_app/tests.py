c8560ce32ae5e63fba830384f5bfb90f3cb70776
unknown <Sw0tty@yandex.ru>
2023-11-02 17:37:52 +0300

    tests update

--- tourism_app/tests.py
+++ tourism_app/tests.py
@@ -3,8 +3,8 @@ from django.urls import reverse
 from rest_framework.test import APITestCase, APIRequestFactory
 from rest_framework import status
 
-from tourism_app.models import Tourist
-from tourism_app.views import SubmitData, ListMountainPasses
+from tourism_app.models import Tourist, MountainPass, MountainCoords, MountainLevel
+from tourism_app.views import SubmitData, ListMountainPasses, DetailMountainPass
 
 
 # class TouristCreateTestCase(APITestCase):
@@ -56,14 +56,46 @@ class MountainPassCreateTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
 
-class FilterMountainsPassesTestCase(APITestCase):
-    def setUp(self) -> None:
-        self.factory = APIRequestFactory()
-        self.view = ListMountainPasses.as_view()
-        self.url = reverse('filter_pass')
+# class FilterMountainsPassesTestCase(APITestCase):
+#     def setUp(self) -> None:
+#         self.factory = APIRequestFactory()
+#         self.view = ListMountainPasses.as_view()
+#         self.url = reverse('filter_pass')
 
-    def test_filter_data(self):
-        email_user = 'vanya@mail.ru'
-        request = self.factory.get(self.url, email_user)
-        response = self.view(request)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+#     def test_filter_data(self):
+#         email_user = {'user_email': 'vanya@mail.ru'}
+#         request = self.factory.get(self.url, email_user)
+#         response = self.view(request)
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+# class PatchMountainPassTestCase(APITestCase):
+#     def setUp(self) -> None:
+#         self.factory = APIRequestFactory()
+#         self.view = DetailMountainPass.as_view()
+#         self.url = 'submitData/1/'
+#         self.mountain = MountainPass.objects.create(
+#             beauty_title= "Дятлова перевал",
+#             title= "п. Дятлова",
+#             other_titles= "Дятлова",
+#             connect= "-"
+#         )
+#         self.coords = MountainCoords.objects.create(
+#             latitude= "1,34G",
+#             longitude= "2,34.324G",
+#             height= 123
+#         )
+#         self.level = MountainLevel.objects.create(
+#             summer="A",
+#             autumn="B",
+#             spring="B",
+#             winter="D"
+#         )
+    
+#     def test_patch_data(self):
+
+#         # sample_data = MountainPass.objects.get(pk=1)
+#         request = self.factory.get(self.url)
+#         response = self.view(request)
+#         response.render()
+#         print(response.content)
