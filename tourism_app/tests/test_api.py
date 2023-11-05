68f007b0973c45995e00d7343a8c2acd50a50d46
Swotty <Sw0tty@yandex.ru>
2023-11-06 00:04:03 +0300

    tests mod

--- tourism_app/tests/test_api.py
+++ tourism_app/tests/test_api.py
@@ -34,10 +34,19 @@ class MountainPassPatchTestCase(APITestCase):
         self.url = reverse('update_pass', args=[1])
         self.sample_pass = TestSetModels().setUp()
 
-    def test_patch_pass(self):
+    def test_patch_pass_good(self):
+        """
+        Request: with GOOD data
+        """
         request = self.factory.patch(self.url, self.sample_pass, format='json')
         response = self.view(request, 1)
         self.assertEqual(response.data['message'], 'Запись обновлена')
+        
+
+    def test_patch_pass_bad(self):
+        """
+        Request: with BAD data
+        """
         self.sample_pass['level']['summer'] = ''
         request = self.factory.patch(self.url, self.sample_pass, format='json')
         response = self.view(request, 1)
@@ -52,13 +61,21 @@ class MountainPassFilterTestCase(APITestCase):
         self.url = reverse('filter_pass')
         self.sample_pass = TestSetModels().setUp()
     
-    def test_filter_pass(self):
+    def test_filter_pass_good(self):
+        """
+        Request: with GOOD data
+        """
         request = self.factory.get(self.url, format='json')
         response = self.view(request)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         request = self.factory.get(f'{self.url}/?user_email={self.user_email}', format='json')
         response = self.view(request)
         self.assertEqual(response.data[0]['user']['email'], self.user_email)
+
+    def test_filter_pass_bad(self):
+        """
+        Request: with BAD data
+        """
         self.user_email = 'notexist@mail.ru'
         request = self.factory.get(f'{self.url}/?user_email={self.user_email}', format='json')
         response = self.view(request)
